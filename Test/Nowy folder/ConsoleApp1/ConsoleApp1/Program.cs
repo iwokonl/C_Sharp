b2a8f33dc31251eab793bf2ef625832cd1125c56
iwokonl <37576228+iwokonl@users.noreply.github.com>
2022-08-11 16:47:42 +0200

    Iwo
    Gotowe

---
+++ Test/Nowy folder/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Program 
+{
+    class Prog
+    {
+        enum XD
+        {
+            Iwo,
+            Marek,
+            Knur=2
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(XD.Knur);
+        }
+    }
+}
